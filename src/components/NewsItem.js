02b1b8fda5ef7fe25bdefa9aa75f71454a87db18
Rahul Shrimali <shrimalirahul00@gmail.com>
2021-10-28 13:43:26 +0530

    States and constructor of class based comp

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
-    render() {
-        return (
-            <div>
-             This is a news item   
-            </div>
-        )
-    }
+ 
+  render() {
+    let {title, description, imageUrl} = this.props;
+    return (
+      <div className = "my-2">
+        <div className="card" style={{width: "18rem"}}>
+          <img src={imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+              {description}
+            </p>
+            <a href="/newsDetail/newsUrl" className="btn btn-primary btn-sm">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default NewsItem
+export default NewsItem;

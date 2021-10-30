f2438e3a76acdfb845a71f7fd7b350b4a950a0a5
Rahul Shrimali <shrimalirahul00@gmail.com>
2021-10-30 11:42:39 +0530

    Getting news by api

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,18 +3,18 @@ import React, { Component } from "react";
 export class NewsItem extends Component {
  
   render() {
-    let {title, description, imageUrl} = this.props;
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
       <div className = "my-2">
         <div className="card" style={{width: "18rem"}}>
           <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title">{title}...</h5>
             <p className="card-text">
-              {description}
+              {description}...
             </p>
-            <a href="/newsDetail/newsUrl" className="btn btn-primary btn-sm">
-              Go somewhere
+            <a rel="noreferrer" href={newsUrl}  target = "_blank" className="btn btn-primary btn-sm">
+              Read more
             </a>
           </div>
         </div>
